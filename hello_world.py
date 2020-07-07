bebb448146c7c33b87844fb29d597f2aec24203c
Donna 'Katie' McCullough <dmccull8@vols.utk.edu>
2020-07-07 16:00:18 -0400

    Messages were added#

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+'''
+
+Hello World file to be used with GIT 
+created by DKM
+
+
+''' 
+
+
+msg1 = "Hello World!"     #message 1
+
+msg2 = " This is my first Git project. I hope it works."
+
+
+print (msg1) 
+
+print (msg1 + msg2)
+
